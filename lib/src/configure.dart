3f8aaaca595ce4de623de207ad85c005c7b9d5c1
Robert Stok <robert.stok20@imperial.ac.uk>
2023-02-05 04:56:56 +0000

    [feat / rs1720] user can now configure options

--- lib/src/configure.dart
+++ lib/src/configure.dart
@@ -1,12 +1,78 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class Configure extends StatelessWidget {
+class Configure extends StatefulWidget {
   const Configure({super.key});
+
+
+
+  @override
+  State<StatefulWidget> createState() => _configureState();
+  
+}
+
+class _configureState extends State<Configure> {
+  bool takingDrugs = false;
+  bool notifyFriends = true;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Spacer(flex: 8),
+                    const Text(
+                        "Are you planning to take drugs?",
+                        style: TextStyle(fontWeight: FontWeight.normal, fontSize: 36, color: Colors.white)
+                    ),
+                    Switch(
+                        value: takingDrugs,
+                        onChanged: (bool value) {
+                          setState(() {
+                            takingDrugs = value;
+                          });
+                        }),
+                    const Spacer(flex: 1),
+                    const Text(
+                        "Would you like to notify a friend?",
+                        style: TextStyle(fontWeight: FontWeight.normal, fontSize: 36, color: Colors.white)
+                    ),
+                    Switch(
+                        value: notifyFriends,
+                        onChanged: (bool value) {
+                          setState(() {
+                            notifyFriends = value;
+                          });
+                        }
+                    ),
+                    const Spacer(flex: 1),
+                    TextField(
+                      decoration: InputDecoration(
+                          hintText: 'friends number'
+                      ),
+                    ),
+                    const Spacer(flex: 1),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => const Configure())
+                          );
+                        },
+                        child: const Text(
+                          "start the night",
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                        )
+                    ),
+                    const Spacer(flex: 8)
+                  ]),
+            )
+        )
+    );
   }
-  
 }
